12f5dc6b9e1a2438fdacdbeeb3dc12095dc64cd2
alexrossetti <alexjoakland@gmail.com>
2018-11-27 15:30:28 +0000

    Updates

---
+++ precache-manifest.4cf53b2956585d1edb673b32d77feae7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d4380b8d6f67d4bd8a2",
+    "url": "/todo/static/css/main.bb77930e.chunk.css"
+  },
+  {
+    "revision": "0d4380b8d6f67d4bd8a2",
+    "url": "/todo/static/js/main.0d4380b8.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/todo/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "1e1f7bdc74a1eb6c4d625535178715ed",
+    "url": "/todo/index.html"
+  }
+];
